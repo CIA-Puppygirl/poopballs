2254875df2652271cb18eed1ae99376a56cc6a94
Soyl <142705938+CIA-Puppygirl@users.noreply.github.com>
2023-08-19 21:29:04 -0700

    Add files via upload

---
+++ poopballs.py
@@ -0,0 +1,17 @@
+poop = True
+balls = None
+orphan = False
+your_mom = None
+gay = None
+fuck = None
+
+if poop:
+    balls()
+
+if orphan:
+    aborted()
+    while True:
+        your_mom()
+        if your_mom == gay:
+            fuck()
+        
