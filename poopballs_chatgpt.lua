2d8842c9faf5833f4b59a1e15e5c9b3f60b629fd
Soyl <142705938+CIA-Puppygirl@users.noreply.github.com>
2023-08-19 21:09:26 -0700

    Add files via upload
    poopballs.lua but "fixed" by chatgpt

---
+++ poopballs_chatgpt.lua
@@ -0,0 +1,23 @@
+-- Variable Declarations
+local poop = true
+local balls = function() end
+local orphan = false
+local your_mom = "some value"
+local gay = "some other value"
+local fuck = function() end
+
+-- First Code Block (Conditional Statement)
+if poop then
+    balls()
+end
+
+-- Second Code Block
+if orphan then
+    aborted()
+    repeat
+        your_mom()
+        if your_mom == gay then
+            fuck()
+        end
+    until false -- A proper condition should be used here
+end
