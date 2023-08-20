778a1e7033101417807d0a10a053d71d2b17aa04
Soyl <142705938+CIA-Puppygirl@users.noreply.github.com>
2023-08-19 21:13:28 -0700

    Add files via upload
    rust chatgpt poopballs

---
+++ poopballs.rs
@@ -0,0 +1,32 @@
+fn balls() {}
+
+fn aborted() {}
+
+fn your_mom() {}
+
+fn fuck() {}
+
+fn main() {
+    let poop = true;
+    let orphan = false;
+    let your_mom = "some value";
+    let gay = "some other value";
+
+    // First Code Block (Conditional Statement)
+    if poop {
+        balls();
+    }
+
+    // Second Code Block
+    if orphan {
+        aborted();
+        loop {
+            your_mom();
+            if your_mom == gay {
+                fuck();
+            }
+            // A proper condition should be used here to terminate the loop
+            break;
+        }
+    }
+}
